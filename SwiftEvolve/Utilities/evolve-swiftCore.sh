725456e1526e9880093e709dfc2e08cff6ce78f2
Brent Royal-Gordon <broyalgordon@apple.com>
2018-12-13 18:33:39 -0800

    Fix various bugs and issues in the driver script
    This has now run through once, although not all in one go!

--- SwiftEvolve/Utilities/evolve-swiftCore.sh
+++ SwiftEvolve/Utilities/evolve-swiftCore.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/caffeinate -i /bin/bash
 
 until [ -e build ]; do
   if [[ "$(pwd)" == "/" ]]; then
@@ -14,7 +14,7 @@ if ! git -C swift diff --exit-code --quiet -- stdlib; then
   exit 1
 fi
 
-BUILD_SCRIPT_ARGS="--build-subdir=buildbot_evolve-swiftCore" $@
+BUILD_SCRIPT_ARGS="--build-subdir=buildbot_evolve-swiftCore" "$@"
 ROOT="$(pwd)"
 BUILD=$ROOT/build/buildbot_evolve-swiftCore
 BUILD_SWIFT=$BUILD/swift-macosx-x86_64
@@ -27,8 +27,8 @@ function run() {
   shift 1
 
   echo "BEGIN: $descString"
-  echo '$' $@
-  if eval $@ ; then
+  echo '$' "$@"
+  if "$@" ; then
     echo "PASS: $descString"
   else
     echo "FAIL: $descString"
@@ -37,44 +37,49 @@ function run() {
 }
 
 function buildSwift() {
-  run "Building Swift with $phase" swift/utils/build-script $BUILD_SCRIPT_ARGS $@
+  run "Building Swift with $phase" swift/utils/build-script $BUILD_SCRIPT_ARGS "$@"
 }
 
 function testSwift() {
-  run "Testing Swift with $phase" llvm/utils/lit/lit.py -sv --param swift_site_config=$BUILD_SWIFT/test-macosx-x86_64/lit.site.cfg $@ swift/test
+  run "Testing Swift with $phase" llvm/utils/lit/lit.py -sv --param swift_site_config=$BUILD_SWIFT/test-macosx-x86_64/lit.site.cfg "$@" swift/test
 }
 
 function evolveSwift() {
   run "Evolving Swift source code" env PATH="$BUILD/swiftpm-macosx-x86_64/x86_64-apple-macosx/debug:$PATH" swift run --package-path $EVOLVE swift-evolve --replace --rules=$EVOLVE/Utilities/swiftCore-exclude.json $ROOT/swift/stdlib/public/core/*.swift
 }
 
+function diffStdlib() {
+  git -C swift diff --minimal -- stdlib >$1
+}
+
 function libs() {
   echo "$BUILD_SWIFT/lib/swift$1$2"
 }
 
-function saveLibs() {
-  rm -r $(libs $1 $2)
-  mv $(libs) $(libs $1 $2)
-}
-
-function mixLibs() {
-  rm -r $(libs $1 $2)
-  run "Copying $1 Modules to $phase" cp -Rc $(libs $1 $1) $(libs $1 $2)
-  run "Copying $2 Binaries to $phase" rsync -av --include '*/' --include '*.dylib' --exclude '*' $(libs $2 $2)/ $(libs $1 $2)
-}
-
 function useLibs() {
-  rm $(libs)
+  rm -rf $(libs)
   ln -s $(libs $1 $2) $(libs)
 }
 
+function saveLibs() {
+  rm -rf $(libs $1 $2)
+  mv $(libs) $(libs $1 $2)
+  useLibs $1 $2
+}
+
+function mixLibs() {
+  rm -rf $(libs $1 $2)
+  run "Copying $1 Modules to $phase" cp -Rc $(libs $1 $1) $(libs $1 $2)
+  run "Copying $2 Binaries to $phase" rsync -ai --include '*/' --include '*.dylib' --exclude '*' $(libs $2 $2)/ $(libs $1 $2)
+}
+
 phase="Current Modules, Current Binaries"
-#buildSwift --llbuild --swiftpm --swiftsyntax
+buildSwift --llbuild --swiftpm --swiftsyntax
 testSwift
 saveLibs 'Current' 'Current'
 
 evolveSwift
-git -C swift diff --minimal -- stdlib >stdlib.diff
+diffStdlib stdlib.diff
 
 phase="Evolved Modules, Evolved Binaries"
 buildSwift
