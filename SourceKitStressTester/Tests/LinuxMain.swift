7c90e39afc365aba3df45183f181ad3c131602a0
Alex Hoppen <ahoppen@apple.com>
2019-11-05 13:47:22 -0800

    Add a dummy LinuxMain.swift to the stress tester
    This allows other projects to test in a unified build.

---
+++ SourceKitStressTester/Tests/LinuxMain.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+// This is a dummpy file to allow running tests of SwiftSyntax on Linux in a 
+// unified build. Running the stress tester on Linux is not supported.
+
+XCTMain({ () -> [XCTestCaseEntry] in
+  return []
+}())
